efc8366e41f9fbca52294ff94a0579eec91c3865
STejaDS <tejaprince369@gmail.com>
2026-07-06 13:50:14 +0530

    until todays created core

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements() -> List[str]:
+
+    requirements_list : List[str] = []
+
+    return requirements_list
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Teja',
+    author_email='tejaprince369@gmail.com',
+    packages=find_packages(),
+    install_requires= get_requirements()
+
+
+)
